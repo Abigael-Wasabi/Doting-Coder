ff2e771c64d0e7ffe4c84e65e0469fd4b19a3686
Abigael-Wasabi <94919325+Abigael-Wasabi@users.noreply.github.com>
2022-04-03 13:14:57 +0300

    lambda func n a dict

---
+++ makefile.py
@@ -0,0 +1,14 @@
+def w(x, r, t): return (x+r)/t
+
+
+print(w(90, 45, 15))
+
+
+Noel = {
+    "age": "17",
+    "height": "165",
+    "weight": "64"
+}
+print(Noel["age"])
+print(Noel["height"])
+print(Noel["weight"])
